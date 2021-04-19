4bc8699e26a30c99ec4d39d9151e72935137e1b2
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-19 16:06:19 -0400

    install styled-components, react-router-dom; in App.js import BrowserRouter from react-router-dom, import Navbar component and render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Let's goooo</h1>
+    <div>
+     <Navbar />
     </div>
   );
 }
