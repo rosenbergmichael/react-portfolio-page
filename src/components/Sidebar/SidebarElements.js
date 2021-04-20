f5ee07b9c6f51eae46144b644d02fc9662771dfa
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-20 11:16:37 -0400

    in SidebarElements.js import Link from react-scroll and react-router-dom, create and export styled components SidebarWrapper, SidebarLink, SideBtnWrap and SidebarRoute

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import {Link as LinkS} from 'react-scroll';
+import {Link as LinkR} from 'react-router-dom';
 import {FaTimes} from 'react-icons/fa';
 
 export const SidebarContainer = styled.aside`
@@ -29,4 +31,51 @@ export const Icon = styled.div`
   font-size: 2rem;
   cursor: pointer;
   outline: none;
+`
+// add below to git push
+export const SidebarWrapper = styled.div`
+  color: #fff;
+`
+
+export const SidebarLink = styled(LinkS)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  text-decoration: none;
+  list-style: none;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    color: #01bf71;
+    transition: 0.2s ease-in-out;
+  }
+`
+
+export const SideBtnWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+export const SidebarRoute = styled(LinkR)`
+  border-radius: 50px;
+  background: #01bf71;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #010606;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  text-decoration: none;
+
+  &:hover {
+    transition: all 0.2s ease-in-out;
+    background: #fff;
+    color: #010606;
+  }
 `
