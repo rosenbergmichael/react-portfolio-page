acf0886901148ca4422467bfaa6b5c25bc115a6a
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-20 10:09:30 -0400

    install react-icons, in Navbar index.js import FaBars from react-icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FaBars} from 'react-icons/fa';
 import {Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks} from './NavbarElements';
 
 const Navbar = () => {
