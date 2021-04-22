0c5d392a666938f87b67d8cec21772a11875fec7
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-22 11:14:21 -0400

    in HeroSection index.js import Video, HeroContainer, HeroBg and VideoBg

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Video from '../../videos/video.mp4';
+import { HeroContainer, HeroBg, VideoBg } from './HeroElements';
 
 const HeroSection = () => {
   return (
     <HeroContainer>
       <HeroBg>
-        <VideoBg src={Video} type='videos/mp4' />
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
     </HeroContainer>
   )
