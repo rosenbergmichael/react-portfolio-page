d4a399d2f4be4c6fe7bbb67aa8f423986959b5d1
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-22 11:00:23 -0400

    in HeroSection index.js create functional HeroSection component, render styled components HeroContainer and HeroBg to be created

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        {/* <ImageBg src={Image} type='img' /> */}
+      </HeroBg>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
