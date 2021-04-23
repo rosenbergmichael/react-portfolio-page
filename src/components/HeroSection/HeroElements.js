58331a0bd9642814fa6f42c7bc6293dfcbb596ec
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-23 11:28:51 -0400

    in HeroElements.js create/export styled components HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward and ArrowRight; import arrow icons from react-icons

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import {MdKeyboardArrowRight, MdArrowForward} from 'react-icons/md';
 
 export const HeroContainer = styled.div`
   background: #0c0c0c;
@@ -22,7 +23,6 @@ export const HeroBg = styled.div`
   overflow: hidden;
 `
 
-// might switch below to image instead of video, make appropriate changes in HeroSection index.js also
 export const VideoBg = styled.video`
   width: 100%;
   height: 100%;
@@ -30,3 +30,61 @@ export const VideoBg = styled.video`
   object-fit: cover;
   background: #232a34;
 `
+
+export const HeroContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const HeroH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`
+
+export const HeroP = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 24px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+  }
+`
+
+export const HeroBtnWrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ArrowForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size: 20px;
+`
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+margin-left: 8px;
+font-size: 20px;
+`
+
