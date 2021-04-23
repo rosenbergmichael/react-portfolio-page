14edf70a13be8cba5530b52b438fbc05b98c0c9d
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-23 11:18:45 -0400

    in HeroSection index.js render styled components HeroContent, HeroH1, HeroP, HeroBtnWrapper and Button

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -8,6 +8,15 @@ const HeroSection = () => {
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
       </HeroBg>
+      <HeroContent>
+        <HeroH1>Michael Rosenberg</HeroH1>
+        <HeroP>Full Stack Software Engineer</HeroP>
+        <HeroBtnWrapper>
+          <Button to='contact'>
+            Contact{hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
     </HeroContainer>
   )
 }
