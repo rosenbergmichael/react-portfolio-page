4ffb8c7ba58eec5080761480a37f44a61c7cb697
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-04-27 16:52:56 -0400

    in Data.js create/export homeObjOne, first section of home page

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'About',
+  headline: 'Full Stack Software Engineer',
+  description: 'tbd',
+  buttonLabel: 'About',
+  imgStart: false,
+  img: require('../../images/svg-1.svg'),
+  alt: 'About pic',
+  dark: true,
+  primary: true,
+  darkText: false
+}
